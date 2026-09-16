395c0a26492f2386a5c446d9db3e9da235fda9de
kosuke kurose <kousuke10010@gmail.com>
2026-09-16 18:25:03 +0900

    敵のAIの登録と初期化を行うGameEnemyInitializerの追加

---
+++ Assets/Script/Application/Enemy/EnemyInitializer.cs
@@ -0,0 +1,60 @@
+using MessagePipe;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace Application.Enemy
+{
+    public class GameEnemyInitializer : IStartable,ITickable
+    {
+        private readonly Game.Enemy.Enemy _enemy;
+        private readonly Presentation.Enemy.EnemyView _enemyView;
+        private readonly EnemyMoveUseCase _enemyMove;
+        private readonly EnemyAttackUseCase _enemyAttack;
+        private readonly EnemyDamageUseCase _enemyDamage;
+        private readonly IPublisher<Framework.Core.Events.EnemyStateChangedEvent> _publisher;
+
+        public GameEnemyInitializer(
+            Game.Enemy.Enemy enemy,
+            Presentation.Enemy.EnemyView enemyView,
+            EnemyMoveUseCase enemyMove,
+            EnemyAttackUseCase enemyAttack,
+            EnemyDamageUseCase enemyDamage,
+            IPublisher<Framework.Core.Events.EnemyStateChangedEvent> publisher
+        )
+        {
+            _enemy = enemy;
+            _enemyView = enemyView;
+            _enemyMove = enemyMove;
+            _enemyAttack = enemyAttack;
+            _enemyDamage = enemyDamage;
+            _publisher = publisher;
+        }
+
+        public void Start() 
+        {
+            _enemyView.InitializeEnemy(_enemy);
+
+            var idleState = new Game.Enemy.EnemyState.EnemyIdleState(_enemy,_enemy.StateMachine,_publisher);
+            var moveState = new Game.Enemy.EnemyState.EnemyChaseState(_enemy,_enemy.StateMachine,_publisher);
+            var attackState = new Game.Enemy.EnemyState.EnemyAttackState(_enemy,_enemy.StateMachine,_publisher);
+            var deathState = new Game.Enemy.EnemyState.EnemyDeathState(_enemy,_enemy.StateMachine,_publisher);
+            
+            _enemy.StateMachine.RegisterState(idleState);
+            _enemy.StateMachine.RegisterState(moveState);
+            _enemy.StateMachine.RegisterState(attackState);
+            _enemy.StateMachine.RegisterState(deathState);
+
+            _enemy.StateMachine.ChangeState<Game.Enemy.EnemyState.EnemyIdleState>();
+        }
+
+        public void Tick()
+        {
+            _enemyDamage.Update();
+            //AiロジックのUpdateを追加予定
+            _enemy.StateMachine.Update(Time.deltaTime);
+        }
+    }
+    
+}
+
+
